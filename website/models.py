67591005074fe1fdccb42ce57bf9bba03cd31101
JC <carlolavaris@gmail.com>
2024-12-10 17:50:57 +0900

    Final Project

--- website/models.py
+++ website/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Record(models.Model):
+	created_at = models.DateTimeField(auto_now_add=True)
+	Title = models.CharField(max_length=50)
+	Author = models.CharField(max_length=50)
+	Booktype = models.CharField(max_length=30)
+	Chapters = models.CharField(max_length=30)
+	Status =  models.CharField(max_length=100) 
+	Genre =  models.CharField(max_length=50)
+	Tag =  models.CharField(max_length=100)
+	
+	def __str__(self):
+		return (f"{self.Title}")
